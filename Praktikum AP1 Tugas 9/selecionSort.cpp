bb72ecd898f47cf889f921bd7abd5b461ddd7b3f
syaqillamaulidiaa <146154351+syaqillamaulidiaa@users.noreply.github.com>
2023-11-29 12:38:29 +0700

    Add files via upload

---
+++ Praktikum AP1 Tugas 9/selecionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void selectionSortDescending(float data[], int n) {
+  for (int i=0; i<n - 1; i++) {
+    int maxIndex = i;
+    for (int j = i+1; j<n; j++) {
+      if (data[j] > data[maxIndex]) {
+        maxIndex = j;
+      }
+    }
+    swap(data[i], data[maxIndex]);
+  }
+}
+
+  int main() {
+  float data[] = {21, 83, 42, 11, 10, 9, 3, 20, 102, 27, 15, 92, 2};
+  int n = sizeof(data) / sizeof(data[0]);
+
+  selectionSortDescending(data, n);
+
+  cout << "Data setelah diurutkan secara menurun: ";
+  for (int i=0; i<n; i++) {
+    cout << data[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
